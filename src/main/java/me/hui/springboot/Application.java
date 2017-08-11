becf3b6f3a07903339b8c10425718c5aa2b4e000
tanhui2333 <tanhui2333@outlook.com>
2017-08-11 10:43:20 +0800

    init

---
+++ src/main/java/me/hui/springboot/Application.java
@@ -0,0 +1,16 @@
+package me.hui.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
